a40386d57180773cd3e8a76fe9b454ba90728086
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-05-08 00:50:07 +0530

    - browserable api added to settings

--- localsecrets/settings.py
+++ localsecrets/settings.py
@@ -130,6 +130,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 0,
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
