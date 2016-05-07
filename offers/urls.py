78c9829381b698d8db98d5f42b137f0b1bee7ada
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-05-08 03:16:56 +0530

    - nearest api added.

--- offers/urls.py
+++ offers/urls.py
@@ -1,8 +1,23 @@
 from offers import views
 from utils.routers import SharedAPIRootRouter
 router = SharedAPIRootRouter()
-
+from django.conf.urls import url, include
 
 router.register(r'offer', views.OfferViewSet)
 router.register(r'offerhistory', views.OfferHistoryViewSet)
 router.register(r'offerreview', views.OfferReviewViewSet)
+router.register(r'nearestoffer', views.NearestOffer)
+
+
+# near_offer = views.NearestOffer.as_view({
+#     'get': 'list',
+# })
+
+
+# urlpatterns = [
+#     url(
+#         regex=r'^nearestoffer',
+#         view=near_offer,
+#         name="near-offer"
+#     ),
+# ]
--- offers/views.py
+++ offers/views.py
@@ -6,6 +6,26 @@ from offers.serializers import (
     OfferHistorySerializer,
     OfferReviewSerializer
 )
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from math import radians, cos, sin, asin, sqrt
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    km = 6367 * c
+    return km
 
 
 class OfferViewSet(viewsets.ModelViewSet):
@@ -45,3 +65,26 @@ class OfferReviewViewSet(viewsets.ModelViewSet):
         if offer_id:
             queryset = queryset.filter(offer__id=offer_id)
         return queryset
+
+
+class NearestOffer(viewsets.ViewSet):
+    queryset = Offer.objects.all()
+    # serializer_class = OfferSerializer
+
+    def list(self, request):
+        params = request.query_params
+        offers = []
+        if params:
+            ulat = float(params['lat'])
+            ulon = float(params['lon'])
+            for offer in Offer.objects.select_related('shop').all():
+                olat = float(offer.shop.latitude)
+                olon = float(offer.shop.longitude)
+                distance = haversine(ulat, ulon, olat, olon)
+                offer_data = OfferSerializer(offer).data
+                offer_data['distace'] = float(distance)
+                offers.append(offer_data)
+
+        return Response(
+            offers,
+            status=status.HTTP_200_OK)
